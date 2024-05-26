08a42cb95e53772fcec63eeb1098673a1810be06
Data4i <paulobiahu@gmail.com>
2024-05-26 20:52:17 +0100

    completed student all_course_details

--- student/urls.py
+++ student/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path("", views.get_courses, name='all_courses')
+    path("", views.get_courses, name='all_courses'),
+    path("all_course_details/<str:course_title>/", views.get_all_course_details, name="all_course_details")
 ]
--- student/views.py
+++ student/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from teacher.models import Course
+
 
 # Create your views here.
 @login_required
 def get_courses(request):
-    return render(request, 'student/home.html')
+    courses = Course.objects.all()
+    context = {'courses': courses}
+    return render(request, 'student/home.html', context)
+
+@login_required
+def get_all_course_details(request, course_title):
+    course = Course.objects.get(course_title = course_title)
+    context = {
+        "course": course,
+    }
+    return render(request, 'student/all_course_details.html', context)
